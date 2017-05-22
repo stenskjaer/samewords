2b16b4dd3e52215f2a56c537be832e89e8e1b396
Michael Stenskjær Christensen <michael.stenskjaer@gmail.com>
2017-05-22 12:33:59 +0200

    Basic handling of complete document.

---
+++ samewords/core.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import samewords.annotate as annotate
+import samewords.document as document
+
+
+def process_document(filename):
+    """The function directing the processing of a document. Return updated document as string."""
+
+    document_content = document.document_content(filename)
+    chunked_document = document.chunk_document(document_content)
+    paragraphs = document.chunk_paragraphs(chunked_document['inside'])
+    updated_paragraphs = [annotate.critical_note_match_replace_samewords(par) for par in paragraphs]
+
+    return chunked_document['before'] + ''.join(updated_paragraphs) + chunked_document['after']
---
+++ test/test_core.py
@@ -0,0 +1,9 @@
+from samewords.core import *
+import samewords.document as document
+
+class TestMainProcessing:
+    filename = './test/documents/da-49-l1q1.tex'
+    processed_content = document.document_content('./test/documents/da-49-l1q1-processed.tex')
+
+    def test_process_document(self):
+        assert process_document(self.filename) == self.processed_content
