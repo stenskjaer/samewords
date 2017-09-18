acfd59b4db78e0fd2a62513ef3538fd229da4108
Michael Stenskjær Christensen <michael.stenskjaer@gmail.com>
2017-09-18 20:09:26 +0200

    Add test for odd custom ellipsis ",-,"

--- samewords/test/test_annotation.py
+++ samewords/test/test_annotation.py
@@ -54,6 +54,7 @@ class TestCritTextObject:
         dash_without_space = r'\edtext{A B C D E}{\lemma{A-E}\Afootnote{A Vat.}}'
         endash = r'\edtext{A B C D E}{\lemma{A – E}\Afootnote{A Vat.}}'
         emdash = r'\edtext{A B C D E}{\lemma{A — E}\Afootnote{A Vat.}}'
+        comma_string = r'\edtext{A B C D E}{\lemma{A ,-, E}\Afootnote{A Vat.}}'
         result = ['A', 'E']
         assert CritText(single_dash).search_words == result
         assert CritText(double_dash).search_words == result
@@ -61,6 +62,7 @@ class TestCritTextObject:
         assert CritText(dash_without_space).search_words == result
         assert CritText(endash).search_words == result
         assert CritText(emdash).search_words == result
+        assert CritText(comma_string).search_words == result
 
     def test_ellipsis_dots(self):
         dots = r'\edtext{A B C D E}{\lemma{A \dots E}\Afootnote{A Vat.}}'
