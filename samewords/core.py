e527f5c6d944c59dcf507d6c331c435a2d9b9e99
Michael Stenskjær Christensen <michael.stenskjaer@gmail.com>
2018-01-24 22:25:39 -0700

    Update the Core module to use new implementation and update tests
    Turned out, of course, that the old implementation was full of errors that the
    new implementation reveals, so the test needed updating.

--- samewords/core.py
+++ samewords/core.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import samewords.annotate as annotate
+from samewords.matcher import Matcher
+from samewords.tokenize import Tokenizer
 import samewords.document as document
 
 
-def process_document(filename):
-    """The function directing the processing of a document. Return updated document as string."""
+def run_annotation(input_text: str) -> str:
+    tokenization = Tokenizer(input_text)
+    matcher = Matcher(tokenization.wordlist, tokenization.registry)
+    words = matcher.annotate()
+    return words.write()
+
+
+def process_document(filename: str) -> str:
+    """The function directing the processing of a document. Return updated
+    document as string. """
 
     document_content = document.document_content(filename)
     chunked_document = document.chunk_document(document_content)
     paragraphs = document.chunk_paragraphs(chunked_document['inside'])
-    updated_paragraphs = [annotate.critical_note_match_replace_samewords(par) for par in paragraphs]
+    updated_paragraphs = [run_annotation(par) for par in paragraphs]
 
-    return chunked_document['before'] + ''.join(updated_paragraphs) + chunked_document['after']
+    return (chunked_document['before'] +
+            ''.join(updated_paragraphs) +
+            chunked_document['after'])
--- samewords/test/test_core.py
+++ samewords/test/test_core.py
@@ -3,7 +3,8 @@ import samewords.document as document
 
 class TestMainProcessing:
     filename = './samewords/test/assets/da-49-l1q1.tex'
-    processed_content = document.document_content('./samewords/test/assets/da-49-l1q1-processed.tex')
+    processed_content = document.document_content(
+        './samewords/test/assets/da-49-l1q1-processed.tex')
 
     def test_process_document(self):
         assert process_document(self.filename) == self.processed_content
