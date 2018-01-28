ff68dc065d48d8080025b8877c22a7ee51e81ea3
Michael Stenskjær Christensen <michael.stenskjaer@gmail.com>
2018-01-27 22:51:15 -0700

    Rewrap the help messages a bit

--- samewords/cli.py
+++ samewords/cli.py
@@ -11,14 +11,15 @@ def parse_arguments():
     parser = argparse.ArgumentParser(
         prog='samewords',
         usage='%(prog)s [options] FILE',
-        description='Annotate potentially ambiguous words in critical text editions '
-                    'made with LaTeX and reledmac.')
+        description='Annotate potentially ambiguous words in critical text '
+                    'editions made with LaTeX and reledmac.')
     parser.add_argument('file', metavar='FILE', type=str, nargs=1,
                         help='Location of local file to be processed.')
-    parser.add_argument('--output', dest='location', action='store',
-                        help="Location of the output. You can specify a filename as part of the "
-                             "address. If you don't do that, the name of the input file will be "
-                             "put in the directory specified. (default: '%(default)s')")
+    parser.add_argument(
+        '--output', dest='location', action='store',
+        help="Location of the output. You can specify a filename as part of the"
+             " address. If you don't do that, the name of the input file will "
+             "be put in the directory specified. (default: '%(default)s')")
     parser.add_argument('-v', '--version', action='version',
                         version='%(prog)s {}'.format(samewords.__version__),
                         help="Show version and exit.")
@@ -39,7 +40,8 @@ def main():
             _, output_filename = os.path.split(filename)
             output_dir = output
         elif os.path.isfile(output):
-            answer = input('The file {} already exists. Overwrite (y/n)? '.format(output))
+            answer = input('The file {} already exists. Overwrite (y/n)?'
+                           .format(output))
             if answer.lower() == 'y':
                 output_dir, output_filename = os.path.split(output)
             else:
